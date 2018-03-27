2cae8693f98ea6a513385fdc6656660eaa36dee3
Henry Zhang <zivers@live.cn>
2018-03-27 16:05:36 +0800

    Add post edit function

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'content'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:pk>/', views.post_view, name='post_view'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/drafts/', views.post_drafts, name='post_drafts'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+
 ]
--- content/views.py
+++ content/views.py
@@ -1,6 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request, 'index.html', {'posts': posts})
+
+def post_view(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'post_view.html', {'post': post})
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('content:post_view', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('content:post_view', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form': form})
+
+def post_drafts(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    return render(request, 'post_drafts.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('content:post_view', pk=pk)
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('content:index')
