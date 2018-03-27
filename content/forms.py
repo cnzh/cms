e413c6ba32422eb11e0ec2c85f0d5f148850d830
Henry Zhang <zivers@live.cn>
2018-03-27 23:27:12 +0800

    front-end optimization

--- content/forms.py
+++ content/forms.py
@@ -1,8 +1,11 @@
-from django import forms
+from django.forms import ModelForm, Textarea
 from .models import Post
 
-
-class PostForm(forms.ModelForm):
+class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+        widgets = {
+            'title': Textarea(attrs={'class': 'form-control', 'rows': 1}),
+            'text': Textarea(attrs={'class': 'form-control', 'rows': 25}),
+        }
